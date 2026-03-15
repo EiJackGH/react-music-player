e838901daa327a993c39b988a66e4e4907f16330
EiJackGH <rahmatnaim.48@gmail.com>
2026-03-15 22:25:26 +0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import MusicPlayer from "./components/MusicPlayer";
+import "./style.css";
+
+function App() {
+  return (
+    <div>
+      <MusicPlayer />
+    </div>
+  );
+}
+
+export default App;
